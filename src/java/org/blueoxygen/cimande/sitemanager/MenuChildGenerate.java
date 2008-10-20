6811048d4421e0791f4a9388b21254769eb771cd
kaskuers <kaskuers@f198d88d-7510-0410-93b4-f2b74cd1fb68>
2008-10-20 08:17:23 +0000

    Menu Workspace on cimande Design is poor, still progress for a better look.

---
+++ src/java/org/blueoxygen/cimande/sitemanager/MenuChildGenerate.java
@@ -0,0 +1,137 @@
+/**
+ * 
+ */
+package org.blueoxygen.cimande.sitemanager;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.blueoxygen.cimande.modulefunction.ModuleFunction;
+import org.blueoxygen.cimande.persistence.PersistenceManager;
+
+/**
+ * @author chibi
+ *
+ */
+public class MenuChildGenerate {
+	
+	private ModuleFunction moduleFunction=null;
+	private String rootId="";
+	private String sId="";
+	private String MenuJS="",variableNode="";
+	private int i, Node,nextLevel,child = 1,size;
+	private PersistenceManager pm;
+	// default variable
+	String mySQL;
+	
+	public MenuChildGenerate (String rootId , String variableNode, int Node,PersistenceManager manager) throws ClassNotFoundException, Exception {
+		this.rootId = rootId;
+		this.sId = rootId;
+		this.moduleFunction = (ModuleFunction) manager.getById(ModuleFunction.class, rootId);	
+		this.Node=Node;
+		this.variableNode=variableNode;
+		this.pm = manager;
+		
+	}
+	
+	/**
+	 * return root id
+	 */
+	public String getRootId()  {
+		return this.rootId;
+	}
+
+	/**
+	 * return moduleFunction
+	 */
+	public ModuleFunction getRoot() {
+		return 	this.moduleFunction;
+	}
+	
+	public int getChildCount()throws ClassNotFoundException, SQLException, Exception {
+	
+		mySQL = "FROM "+ModuleFunction.class.getName()+" mf WHERE mf.logInformation.activeFlag='1' AND mf.moduleFunction.id='"+this.sId+"'";
+		List temp = new ArrayList();
+		temp = pm.getList(mySQL,null,null);
+		return temp.size();
+	}
+	
+	public String getMenuJS(int nextLevel) throws ClassNotFoundException, SQLException, Exception {
+		this.nextLevel = nextLevel;
+		
+		MenuChildGenerate childGenerate;
+
+		String sParentId="";
+		//i=1;
+		mySQL = "FROM mf in " + ModuleFunction.class + " WHERE mf.moduleFunction.id = '" + this.sId + "' ORDER BY(mf.description)";
+		List<ModuleFunction> modules = new ArrayList<ModuleFunction>();
+		modules = (List<ModuleFunction>)pm.getList(mySQL,null,null);
+		i = modules.size();
+		size = 0;
+		
+		for(ModuleFunction mf : modules){
+			
+			sParentId = mf.getId();
+			
+			ModuleFunction mFunction = (ModuleFunction) pm.getById(ModuleFunction.class, sParentId);
+
+			int totalChild ;
+			childGenerate = new MenuChildGenerate(mFunction.getId(), variableNode+"_"+Node, i, pm);
+
+			totalChild = childGenerate.getChildCount();
+			
+			
+			if(totalChild > 0){
+				MenuJS = MenuJS + "<li class=\"parent level"+nextLevel+"\"><span>"+mf.getDescription()+"</span><ul>";
+				nextLevel = nextLevel + 1;
+				MenuJS = MenuJS + childGenerate.getMenuJS(nextLevel);
+				MenuJS = MenuJS + "</ul></li>";
+				
+					
+			}else{
+				child = child + nextLevel;
+				String sActionFlag="", sUrlAction="";
+				
+				sActionFlag=String.valueOf(mf.getModuleDescriptor().getTypeFlag());
+				
+				if (sActionFlag.equals("1")) {
+					sUrlAction = mf.getModuleDescriptor().getUrlAction()+"&";
+				} else if (sActionFlag.equals("2")) {
+					sUrlAction = "../module/"+mf.getModuleDescriptor().getName()+"/";
+				} else if (sActionFlag.equals("3")){
+					sUrlAction = "../module/window/genform.action?window.id="+mf.getModuleDescriptor().getWindow().getId()+"&";
+				}else {
+					sUrlAction = "../descriptor/"+mf.getModuleDescriptor().getUrlDescriptor()+"?";
+				}
+				
+				MenuJS = MenuJS + "<li onmouseover=\"Element.addClassName(this,'over')\" onmouseout=\"Element.removeClassName(this,'over')\" class=\"parent level"+nextLevel+"\"><span><a href=\"#\" onclick=\"return false\">"+ mf.getDescription() +"</a></span><ul>";
+				
+				if (sActionFlag.equals("2")) {
+					
+					MenuJS = MenuJS + "<li class=\"level"+child+"\"><a href=\""+sUrlAction+"create.action\" target=\"main\">New</a></li>";
+				} else {
+					MenuJS = MenuJS + "<li class=\"level"+child+"\"><a href=\""+sUrlAction+"action=new\">New</a></li>";
+				}
+				
+				if (sActionFlag.equals("2")) {
+					
+					MenuJS = MenuJS + "<li class=\"last level"+child+"\"><a href=\""+sUrlAction+"filter.action\" target=\"main\">Search</a></li>";
+				} else {
+					MenuJS = MenuJS + "<li class=\"last level"+child+"\"><a href=\""+sUrlAction+"action=search\">Search</a></li>";
+				}
+				
+				MenuJS = MenuJS + "</ul></li>";
+				child = 1;
+				
+								
+			}
+			size++;
+		}
+		//System.out.println("Size :"+size+", i :"+i);
+		nextLevel = 0;
+		this.nextLevel = nextLevel;
+		//System.out.println(nextLevel);
+		return MenuJS;
+	}
+}
---
+++ src/java/org/blueoxygen/cimande/sitemanager/MenuGenerator.java
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+package org.blueoxygen.cimande.sitemanager;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.struts2.ServletActionContext;
+import org.blueoxygen.cimande.CimandeAction;
+import org.blueoxygen.cimande.role.RolePrivilage;
+import org.blueoxygen.cimande.role.RoleSite;
+import org.blueoxygen.cimande.role.RoleSitePrivilage;
+import org.blueoxygen.cimande.security.LoginFilter;
+import org.blueoxygen.cimande.security.User;
+
+/**
+ * @author chibi
+ * 
+ */
+public class MenuGenerator extends CimandeAction {
+	private String roleId = "";
+	private String siteId = "";
+	private String menu_script = "";
+
+	public String execute(){
+		
+		String mySQL;
+		int total_role_site = 0;
+		String variableNode = "menu";
+		String MenuJS = "";
+		
+		MenuChildGenerate MenuChild;
+		
+		try {
+			User us = getCurrentUser();
+			this.roleId = us.getRole().getId();
+			this.siteId = (String) ServletActionContext.getRequest().getSession().getAttribute(LoginFilter.LOGIN_CIMANDE_SITE);
+			
+			mySQL = "FROM " + RoleSite.class.getName() + " tmp WHERE tmp.role.id='" + this.roleId + "' AND tmp.site.id='" + this.siteId + "'";
+			List<RoleSite> temp = new ArrayList<RoleSite>();
+			temp = manager.getList(mySQL, null, null);
+			total_role_site = temp.size();
+			
+			int iFirstNode = 0;
+			
+			if (total_role_site > 0) {
+				mySQL = "FROM tmp in " + RoleSitePrivilage.class + " WHERE tmp.roleSite.site.id = '" + this.siteId + "' AND tmp.roleSite.role.id = '" + this.roleId + "' ORDER BY (tmp.moduleFunction.description)";
+				List<RoleSitePrivilage> rsp = new ArrayList<RoleSitePrivilage>();
+				rsp = (List<RoleSitePrivilage>) manager.getList(mySQL, null,null);
+				
+				for (RoleSitePrivilage tmp : rsp) {
+					MenuChild = new MenuChildGenerate(tmp.getModuleFunction().getId(), variableNode, iFirstNode, manager);
+					MenuJS = MenuJS + "<li onmouseover=\"Element.addClassName(this,'over')\" onmouseout=\"Element.removeClassName(this,'over')\" class=\"parent level"+iFirstNode+"\"><span><a href=\"#\" onclick=\"return false\">"+tmp.getModuleFunction().getDescription()+"</a></span><ul>";
+					
+					MenuJS = MenuJS + MenuChild.getMenuJS(iFirstNode);
+					MenuJS = MenuJS + "</ul></li>";
+					
+					//iFirstNode++;
+				}
+			}else{
+				mySQL = "FROM tmp in " + RolePrivilage.class + " WHERE tmp.role.id='" + this.roleId + "' ORDER BY (tmp.moduleFunction.description)";
+				List<RolePrivilage> rp = new ArrayList<RolePrivilage>();
+				rp = (List<RolePrivilage>) manager.getList(mySQL, null, null);
+				
+				for (RolePrivilage tmp : rp) {
+					MenuChild = new MenuChildGenerate(tmp.getModuleFunction().getId(), variableNode, iFirstNode, manager);
+					MenuJS = MenuJS + "<li class=\"parent level0\"><span>"+tmp.getModuleFunction().getDescription()+"</span><ul>";
+					
+					MenuJS = MenuJS + MenuChild.getMenuJS(iFirstNode);
+					MenuJS = MenuJS + "</ul></li>";
+				}
+			}
+			menu_script = MenuJS;
+			return SUCCESS;
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ERROR;
+		
+	}
+
+	/**
+	 * @return the menu_script
+	 */
+	public String getMenu_script() {
+		return menu_script;
+	}
+
+	/**
+	 * @param menu_script the menu_script to set
+	 */
+	public void setMenu_script(String menu_script) {
+		this.menu_script = menu_script;
+	}
+	
+	
+}
